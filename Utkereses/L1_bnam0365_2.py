dad910117a57a3d8eca7c527bf85e463f0fdac11
Biró Norbert <bironorbi0356@gmail.com>
2025-03-15 13:25:18 +0200

    Minor fix

--- Utkereses/L1_bnam0365_2.py
+++ Utkereses/L1_bnam0365_2.py
@@ -5,18 +5,13 @@ def distance(x1, y1, z1, x2, y2, z2):
     return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
 
 
-def manhattan(x1, y1, z1, x2, y2, z2):
-    return abs(x1 - x2) + abs(y1 - y2) + abs(z1 - z2)
-
-
 def heuristic_function_distance(p1, p2):
     return distance(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z)
 
 
-def heuristic_function_manhattan(p1, p2):
-    return manhattan(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z)
+def heuristic_function_minimal_step(p1, p2):
+    return minimal_step(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z)
 
-def minimal_step(x1,y1,z1,x2,y2,z2):
-    if abs(x1 - x2) < 2 and abs(y1- y2) < 2:
-        return 1
-    return 0
+
+def minimal_step(x1, y1, z1, x2, y2, z2):
+    return max(abs(x1 - x2), abs(y1 - y2))
