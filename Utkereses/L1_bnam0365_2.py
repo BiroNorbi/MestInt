6e5c5b68821c8e5ad05f06d66d5372e2feefc77d
Biró Norbert <bironorbi0356@gmail.com>
2025-03-15 12:59:39 +0200

    a and b finished

--- Utkereses/L1_bnam0365_2.py
+++ Utkereses/L1_bnam0365_2.py
@@ -15,3 +15,8 @@ def heuristic_function_distance(p1, p2):
 
 def heuristic_function_manhattan(p1, p2):
     return manhattan(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z)
+
+def minimal_step(x1,y1,z1,x2,y2,z2):
+    if abs(x1 - x2) < 2 and abs(y1- y2) < 2:
+        return 1
+    return 0
--- Utkereses/L1_bnam0365_6.py
+++ Utkereses/L1_bnam0365_6.py
@@ -1,5 +1,5 @@
 from L1_bnam0365_1 import read_surface, read_end_points, a_star, write_to_output
-from L1_bnam0365_2 import heuristic_function_manhattan
+from L1_bnam0365_2 import heuristic_function_manhattan, distance, minimal_step
 from L1_bnam0365_4 import visualize
 
 
@@ -7,7 +7,7 @@ def main():
     matrix = read_surface("surface_100x100.txt", 100, 100)
     start_point, end_point = read_end_points("surface_100x100.end_points.txt", matrix)
 
-    path = a_star(matrix, start_point, end_point,heuristic=heuristic_function_manhattan)
+    path = a_star(matrix, start_point, end_point,heuristic=heuristic_function_manhattan, cost_function=minimal_step)
     write_to_output("output_b.txt", path)
     visualize(matrix, path,"b")
 
